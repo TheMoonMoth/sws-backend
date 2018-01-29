c40c09f14e827d8ee136f1d0fad907a23adf4cba
Kyle Weintraub <Weintraub.Kyle@gmail.com>
2018-01-29 11:14:18 -0700

    new routes

--- app.js
+++ app.js
@@ -1,14 +1,33 @@
 const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
+const queries = require("./queries")
 
 const app = express()
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get("/stories", (request, response) => {
+app.get("/", (request, response) => {
   response.json("Welcome.")
+})
+
+app.get("/stories", (request, response) => {
+  queries
+    .listStories()
+    .then(stories => {
+      response.json({ stories })
+    })
+    .catch(console.error)
+})
+
+app.get("/authors", (request, response) => {
+  queries
+    .listAuthors()
+    .then(authors => {
+      response.json({ authors })
+    })
+    .catch(console.error)
 })
 
 module.exports = app
---
+++ queries.js
@@ -0,0 +1,36 @@
+const database = require("./dbknextion.js");
+
+module.exports = {
+    listStories(){
+      return database("stories")
+        .select()
+    },
+    listAuthors(){
+      return database("authors")
+        .select()
+    },
+    read(id){
+      return database("stories")
+        .select()
+        .where("id", id)
+        .first()
+    },
+    create(story){
+      return database("stories")
+        .insert(story)
+        .returning("*")
+        .then(record => record[0])
+    },
+    update(id, story){
+      return database("stories")
+        .update(story)
+        .where("id", id)
+        .returning("*")
+        .then(record => record[0])
+    },
+    delete(id){
+      return database("stories")
+        .delete()
+        .where("id", id)
+    }
+}
